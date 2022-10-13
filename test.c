7dbe75d69e0f9d2b9e35370af4dfcfd5df0560cb
Airloss <star.s.fan@outlook.com>
2022-10-13 15:04:40 +0900

    gitignore

--- test.c
+++ test.c
@@ -7,7 +7,7 @@ int main()
     int ret;
     const uint8_t buffer[3] = {0x50,0x10,0xf0};
     char *base64;
-    ret = bin_to_b64(&buffer, sizeof(buffer) / sizeof(buffer[0]), base64, 2048);
+    ret = bin_to_b64(buffer, sizeof(buffer) / sizeof(buffer[0]), base64, 2048);
     printf("End: %d\r\n", ret);
     base64[ret] = 0;
     printf("End: %s\r\n", base64);
